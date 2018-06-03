0c6b59c4c7bbdfeecc6a24837208ce0dde43d2d5
Ting <chanyt0216@gmail.com>
2018-06-03 15:15:28 +0800

    updating lifecycle hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -26,6 +26,27 @@ class App extends Component {
     console.log('[App.js] Inside componentDidMount');
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log(
+      '[UPDATE App.js] Inside shouldComponentUpdate',
+      nextProps,
+      nextState
+    );
+    return nextProps.persons !== this.props.persons;
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log(
+      '[UPDATE App.js] Inside componentWillUpdate',
+      nextProps,
+      nextState
+    );
+  }
+
+  componentDidUpdate() {
+    console.log('[UPDATE App.js] Inside componentDidUpdate');
+  }
+
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
       return p.id === id;
